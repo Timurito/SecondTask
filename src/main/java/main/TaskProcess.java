097c7c0c2b32cb0f47e881257f92648469aa2a50
Timur Ashikhmin <tashikhmin@tsconsulting.ru>
2017-12-20 11:57:20 +0300

    renamed some variables for better readability

--- src/main/java/main/TaskProcess.java
+++ src/main/java/main/TaskProcess.java
@@ -8,49 +8,49 @@ import java.util.*;
 import static util.Constants.*;
 
 public class TaskProcess {
-    private BufferedWriterWrap bw;
-    private List<FileString> fileAAL = new ArrayList<>();
-    private List<FileString> fileBAL = new ArrayList<>();
-    private List<FileString> fileALL = new LinkedList<>();
-    private List<FileString> fileBLL = new LinkedList<>();
-    private Map<Integer, List<String>> fileAMap = new HashMap<>();
-    private Map<Integer, List<String>> fileBMap = new HashMap<>();
+    private BufferedWriterWrap bufferedWriter;
+    private List<FileString> arrayListA = new ArrayList<>();
+    private List<FileString> arrayListB = new ArrayList<>();
+    private List<FileString> linkedListA = new LinkedList<>();
+    private List<FileString> linkedListB = new LinkedList<>();
+    private Map<Integer, List<String>> hashMapA = new HashMap<>();
+    private Map<Integer, List<String>> hashMapB = new HashMap<>();
 
     public TaskProcess(String inputfilename1, String inputfilename2, String outputfilename) {
-        if ((bw = new BufferedWriterWrap(outputfilename)) == null) {
+        if ((bufferedWriter = new BufferedWriterWrap(outputfilename)) == null) {
             return;
         }
-        bw.write("Reading file...\n");
+        bufferedWriter.write("Reading file...\n");
         if (!readFile(inputfilename1, true) || !readFile(inputfilename2, false)) {
             return;
         }
-        bw.write("Successfully read info:\nFile A:\n");
-        for (FileString fs : fileAAL) {
-            bw.write(fs.toString());
+        bufferedWriter.write("Successfully read info:\nFile A:\n");
+        for (FileString fs : arrayListA) {
+            bufferedWriter.write(fs.toString());
         }
-        bw.write("\nFile B:\n");
-        for (FileString fs : fileBAL) {
-            bw.write(fs.toString());
+        bufferedWriter.write("\nFile B:\n");
+        for (FileString fs : arrayListB) {
+            bufferedWriter.write(fs.toString());
         }
-        bw.write("\nArrayList result:\n");
+        bufferedWriter.write("\nArrayList result:\n");
         processDataArrayList();
-        fileALL.sort(Comparator.comparingInt(FileString::getId));
-        fileBLL.sort(Comparator.comparingInt(FileString::getId));
-        bw.write("\nLinkedList result:\n");
+        linkedListA.sort(Comparator.comparingInt(FileString::getId));
+        linkedListB.sort(Comparator.comparingInt(FileString::getId));
+        bufferedWriter.write("\nLinkedList result:\n");
         processDataLinkedList();
-        bw.write("\nHashMap result:\n");
+        bufferedWriter.write("\nHashMap result:\n");
         processDataMap();
-        bw.close();
+        bufferedWriter.close();
     }
 
     private void processDataArrayList() {
-        for (int i = 0; i < fileAAL.size(); i++) {
-            FileString fs = fileAAL.get(i);
-            if (fileBAL.contains(fs) && fileAAL.indexOf(fs) == i) {
-                for (int j = i; j < fileAAL.size(); j++) {
-                    for (FileString aFileBAL : fileBAL) {
-                        if (fs.equals(fileAAL.get(j)) && fs.equals(aFileBAL)) {
-                            bw.write(fs.getId() + " " + fileAAL.get(j).getValue() + " " + aFileBAL.getValue() + '\n');
+        for (int i = 0; i < arrayListA.size(); i++) {
+            FileString fs = arrayListA.get(i);
+            if (arrayListB.contains(fs) && arrayListA.indexOf(fs) == i) {
+                for (int j = i; j < arrayListA.size(); j++) {
+                    for (FileString aFileBAL : arrayListB) {
+                        if (fs.equals(arrayListA.get(j)) && fs.equals(aFileBAL)) {
+                            bufferedWriter.write(fs.getId() + " " + arrayListA.get(j).getValue() + " " + aFileBAL.getValue() + '\n');
                         }
                     }
                 }
@@ -59,14 +59,14 @@ public class TaskProcess {
     }
 
     private void processDataLinkedList() {
-        ListIterator<FileString> itA = fileALL.listIterator();
-        ListIterator<FileString> itB = fileBLL.listIterator();
+        ListIterator<FileString> itA = linkedListA.listIterator();
+        ListIterator<FileString> itB = linkedListB.listIterator();
         while (itA.hasNext()) {
             FileString fsA = itA.next();
             while (itB.hasNext()) {
                 FileString fsB = itB.next();
                 if (fsA.equals(fsB)) {
-                    bw.write(fsA.getId() + " " + fsA.getValue() + " " + fsB.getValue() + '\n');
+                    bufferedWriter.write(fsA.getId() + " " + fsA.getValue() + " " + fsB.getValue() + '\n');
                 } else {
                     if (fsA.getId() < fsB.getId()) {
                         itB.previous();
@@ -87,11 +87,11 @@ public class TaskProcess {
     }
 
     private void processDataMap() {
-        for (Integer iA : fileAMap.keySet()) {
-            if (fileBMap.containsKey(iA)) {
-                for (String sA : fileAMap.get(iA)) {
-                    for (String sB : fileBMap.get(iA)) {
-                        bw.write(iA + " " + sA + " " + sB + '\n');
+        for (Integer iA : hashMapA.keySet()) {
+            if (hashMapB.containsKey(iA)) {
+                for (String sA : hashMapA.get(iA)) {
+                    for (String sB : hashMapB.get(iA)) {
+                        bufferedWriter.write(iA + " " + sA + " " + sB + '\n');
                     }
                 }
             }
@@ -99,36 +99,35 @@ public class TaskProcess {
     }
 
     private boolean readFile(String filename, boolean isFirst) {
-        try (BufferedReader r = new BufferedReader(new FileReader((filename)))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader((filename)))) {
             String temp;
-            while ((temp = r.readLine()) != null) {
+            while ((temp = bufferedReader.readLine()) != null) {
                 String[] stringParts = temp.split(",");
-                Integer id = parseId(stringParts[ID_PART]);
-                if (stringParts.length != STRING_PARTS_NUM || id == null) {
-                    bw.write("[ERROR] Failed to parse line: " + temp + "\n");
+                Integer id;
+                if (stringParts.length != STRING_PARTS_NUM || (id = parseId(stringParts[ID_PART])) == null) {
+                    bufferedWriter.write("[ERROR] Failed to parse line: " + temp + "\n");
                     continue;
                 }
-
-                FileString fs = new FileString(id, stringParts[VALUE_PART]);
+                FileString newFileString = new FileString(id, stringParts[VALUE_PART]);
                 if (isFirst) {
-                    fileAAL.add(fs);
-                    fileALL.add(fs);
-                    if (!fileAMap.containsKey(id)) {
-                        fileAMap.put(id, new ArrayList<>());
+                    arrayListA.add(newFileString);
+                    linkedListA.add(newFileString);
+                    if (!hashMapA.containsKey(id)) {
+                        hashMapA.put(id, new ArrayList<>());
                     }
-                    fileAMap.get(id).add(stringParts[VALUE_PART]);
+                    hashMapA.get(id).add(stringParts[VALUE_PART]);
                 } else {
-                    fileBAL.add(fs);
-                    fileBLL.add(fs);
-                    if (!fileBMap.containsKey(id)) {
-                        fileBMap.put(id, new ArrayList<>());
+                    arrayListB.add(newFileString);
+                    linkedListB.add(newFileString);
+                    if (!hashMapB.containsKey(id)) {
+                        hashMapB.put(id, new ArrayList<>());
                     }
-                    fileBMap.get(id).add(stringParts[VALUE_PART]);
+                    hashMapB.get(id).add(stringParts[VALUE_PART]);
                 }
             }
             return true;
         } catch (IOException e) {
-            bw.write("[ERROR] IO error with input file operation.");
+            bufferedWriter.write("[ERROR] IO error with input file operation.");
             return false;
         }
     }
@@ -144,10 +143,10 @@ public class TaskProcess {
     }
 
     class BufferedWriterWrap {
-        private BufferedWriter bw;
+        private BufferedWriter bufferedWriter;
         public BufferedWriterWrap(String filename) {
             try {
-                bw = new BufferedWriter(new FileWriter(filename));
+                bufferedWriter = new BufferedWriter(new FileWriter(filename));
             } catch (IOException e) {
                 System.err.println("[ERROR] Failed to open/create output file");
             }
@@ -155,16 +154,16 @@ public class TaskProcess {
 
         public void write(String msg) {
             try {
-                bw.write(msg);
+                bufferedWriter.write(msg);
             } catch (IOException e) {
                 System.err.println("[ERROR] Failed writing to output file");
             }
         }
 
         public void close() {
-            if (bw != null) {
+            if (bufferedWriter != null) {
                 try {
-                    bw.close();
+                    bufferedWriter.close();
                 } catch (IOException e) {
                     System.err.println("[ERROR] Failed closing output file");
                 }
